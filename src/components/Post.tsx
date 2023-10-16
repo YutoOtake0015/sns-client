5b0304e991632fbd79d689abe3c4aaf4d0efe22d
OtakeYuto <yuto.programming@gmail.com>
2023-10-16 23:56:48 +0900

    done

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -1,5 +1,6 @@
 import { PostType } from "@/types";
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 type Props = {
@@ -9,19 +10,19 @@ type Props = {
 const Post = (props: Props) => {
   const { post } = props;
 
-  console.log("ログ: ", post.author);
-
   return (
     <div className="bg-white shadow-md rounded p-4 mb-4">
       <div className="mb-4">
         <div className="flex items-center mb-2">
-          <Image
-            className="w-10 h-10 rounded-full mr-2"
-            src={post.author.profile?.profileImageUrl}
-            alt="User Avatar"
-            width={100}
-            height={100}
-          />
+          <Link href={`/profile/${post.authorId}`}>
+            <Image
+              className="w-10 h-10 rounded-full mr-2"
+              src={post.author.profile?.profileImageUrl}
+              alt="User Avatar"
+              width={100}
+              height={100}
+            />
+          </Link>
           <div>
             <h2 className="font-semibold text-md">{post.author?.username}</h2>
             <p className="text-gray-500 text-sm">
